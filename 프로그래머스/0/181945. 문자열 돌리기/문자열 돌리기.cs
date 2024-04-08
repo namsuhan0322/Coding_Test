5e10b1b640c7552c107e9a001e10a848480df181
Namsuhan <142658057+namsuhan0322@users.noreply.github.com>
2024-04-08 18:57:44 +0900

    [level 0] Title: 문자열 돌리기, Time: 17.10 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String s;
+
+        Console.Clear();
+        s = Console.ReadLine();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            char j = s[i];
+            Console.WriteLine(j);
+        }
+    }
+}
